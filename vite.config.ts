f754cfe4748be77256d3a17b60f2058e229d25d0
Vaibhav <147196315+CharVaibhav@users.noreply.github.com>
2025-04-19 23:19:27 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -39,10 +39,10 @@ export default defineConfig({
       output: {
         assetFileNames: 'assets/[name].[hash][extname]',
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
+          vendor: ['react', 'react-dom', 'react-router-dom', 'sonner'],
         },
       },
-      external: ['lovable-tagger', '@tanstack/react-query', '@radix-ui/react-tooltip', 'sonner', 'react-type-animation', 'react-fast-marquee', '@radix-ui/react-slider', '@radix-ui/react-scroll-area', '@radix-ui/react-separator', 'pg', 'uuid'],
+      external: ['lovable-tagger', '@tanstack/react-query', '@radix-ui/react-tooltip', 'react-type-animation', 'react-fast-marquee', '@radix-ui/react-slider', '@radix-ui/react-scroll-area', '@radix-ui/react-separator', 'pg', 'uuid'],
     }
   },
   define: {
